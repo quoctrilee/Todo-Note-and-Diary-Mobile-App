45fd7450e9aaa4b2b51843927b1b71a507f7d7b9
Le Quoc Tri <trilq.05@gmail.com>
2026-07-02 12:43:41 +0700

    refactor simplify dependency di architecture for auth

--- app/src/main/java/com/example/todonotediary/domain/usecase/auth/AuthUseCases.kt
+++ app/src/main/java/com/example/todonotediary/domain/usecase/auth/AuthUseCases.kt
@@ -2,8 +2,9 @@ package com.example.todonotediary.domain.usecase.auth
 
 import com.example.todonotediary.domain.usecase.user.GetUserDataUseCase
 import com.example.todonotediary.domain.usecase.user.UpdateUserAvatarUseCase
+import javax.inject.Inject
 
-data class AuthUseCases(
+data class AuthUseCases @Inject constructor(
     val getCurrentUser: GetCurrentUserUseCase,
     val signInWithGoogle: SignInWithGoogleUseCase,
     val loginWithEmail: LoginWithEmailUseCase,
